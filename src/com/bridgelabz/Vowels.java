6d8d2b1e7cf288e23cb3fee3761b8199533a9200
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-09 00:46:18 +0530

    Added: Check if Vowels or not Program

---
+++ src/com/bridgelabz/Vowels.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'o';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println("Alphabet " +ch+ " is vowel");
+        else
+            System.out.println("Alphabet " +ch+ " is consonant");
+	}
+}
